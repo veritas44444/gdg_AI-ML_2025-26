073e2ddbc3d61ff88ab373d6f6057fb7e9d46c51
Ewenetu Haile <ewenetuhaile4@gmail.com>
2025-11-29 18:26:40 +0300

    message

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# gdg_AI-ML_2025-26
+This is the first assignment within the AI/ML gorup found within GDG.
+It centered around the use of built-in maths functions, i.e sum, mean and maximum value.
---
+++ gdg_task1.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+num_list = [12, 34, 56, 67, 78 ]
+
+num_array = np.array(num_list) #convert list to numarray
+
+Sum = np.sum(num_array) #find sum
+Average = np.mean(num_array) #find mean
+Max_num = np.max(num_array) #find maximum value
+
+print(f'The sum of the 5 numbers is: {Sum}')
+print(f'The average of the 5 numbers is: {Average}')
+print(f'The maximum value of the 5 numbers is: {Max_num}')
+
+
