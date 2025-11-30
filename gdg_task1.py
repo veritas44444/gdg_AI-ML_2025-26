8cd731c9d9b9447a056105236e5dc321a895d95f
Ewenetu Haile <ewenetuhaile4@gmail.com>
2025-11-30 21:05:56 +0300

    testing commit

--- gdg_task1.py
+++ gdg_task1.py
@@ -5,7 +5,7 @@ num_list = [12, 34, 56, 67, 78 ]
 num_array = np.array(num_list) #convert list to numarray
 
 Sum = np.sum(num_array) #find sum
-Average = np.mean(num_array) #find mean
+Average = np.mean(num_array) #find means
 Max_num = np.max(num_array) #find maximum value
 
 print(f'The sum of the 5 numbers is: {Sum}')
